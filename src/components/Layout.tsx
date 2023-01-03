b5181db3926266a62cb18ad087751672754e74c8
benjaminsulh <slh.benjamin@gmail.com>
2023-01-03 15:28:28 +0700

    created grid layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,60 @@
-import Card from './Card';
-
 import eiffelTower from '../resources/eiffelTower.jpg';
 import parisArc from '../resources/parisArc.jpg';
+import coloseum from '../resources/coloseum.jpg';
+import goldenGate from '../resources/goldenGate.jpg';
+import taj from '../resources/taj.jpg';
 
 function Layout() {
     return (
         <div className='layout'>
-            <img className='standard-image' src={eiffelTower}/>
-            <img className='standard-image' src={parisArc}/>
+            <div>
+                <img className='standard-image' src={eiffelTower}/>
+            </div>
+
+            <div>
+                <img className='standard-image' src={parisArc}/>
+            </div>
+            <div>
+                <img className='standard-image' src={coloseum}/>
+
+            </div>
+            <div>
+                <img className='standard-image' src={goldenGate}/>
+            </div>
+            <div>
+                <img className='standard-image' src={taj}/>
+            </div>
+
+            <div>
+                <img className='standard-image' src={eiffelTower}/>
+            </div>
+            <div>
+                <img className='standard-image' src={parisArc}/>
+            </div>
+            <div>
+                <img className='standard-image' src={coloseum}/>
+
+            </div>
+            <div>
+                <img className='standard-image' src={goldenGate}/>
+            </div>
+            <div>
+                <img className='standard-image' src={taj}/>
+            </div>
+
+            <div>
+                <img className='standard-image' src={parisArc}/>
+            </div>
+            <div>
+                <img className='standard-image' src={coloseum}/>
+
+            </div>
+            <div>
+                <img className='standard-image' src={goldenGate}/>
+            </div>
+            <div>
+                <img className='standard-image' src={taj}/>
+            </div>
         </div>
     )
 }
